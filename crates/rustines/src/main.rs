977d85757ea952f503f3424c8a435a48e7b2602b
Davide Canton <davide.canton5@gmail.com>
2026-09-26 19:49:18 +0200

    main logic improvements

--- crates/rustines/src/main.rs
+++ crates/rustines/src/main.rs
@@ -43,7 +43,8 @@ struct AppState {
     key_map1: KeyMap,
     key_map2: KeyMap,
 
-    pattern_window: Option<PatternTableWindow>,
+    pattern_window: PatternTableWindow,
+    main_window: Arc<Window>,
 
     pause: bool,
 }
@@ -62,7 +63,7 @@ pub fn main() {
     let event_loop = EventLoop::new().unwrap();
 
     let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-    let window = Arc::new(
+    let main_window = Arc::new(
         WindowBuilder::new()
             .with_title("Rustines")
             .with_inner_size(size)
@@ -71,20 +72,52 @@ pub fn main() {
             .unwrap(),
     );
 
-    let renderer = create_renderer(Arc::clone(&window)).unwrap();
+    let renderer = create_renderer(Arc::clone(&main_window)).unwrap();
 
+    let mut app_state = init_app_state(rom, main_window, renderer, args.trace_boot);
+
+    let mut input = WinitInputHelper::new();
+
+    let _ = event_loop.run(|event, elwt| {
+        // update logic
+        if input.update(&event) {
+            update_logic(&input, elwt, &mut app_state);
+        }
+
+        // dispatch events to windows
+        match event {
+            Event::WindowEvent { window_id, event } if window_id == app_state.main_window.id() => {
+                main_window_event(&mut app_state, event)
+            }
+            Event::WindowEvent { window_id, event }
+                if app_state.pattern_window.is_event(window_id) =>
+            {
+                pattern_window_event(&mut app_state.pattern_window, event)
+            }
+
+            _ => {}
+        }
+    });
+}
+
+fn init_app_state(
+    rom: rustines_core::NesRom,
+    main_window: Arc<Window>,
+    renderer: PixelsRenderer,
+    trace_boot: bool,
+) -> AppState {
     let ppu = core::Ppu::new(Box::new(renderer));
     let apu = core::Apu::default();
 
     let mut bus = core::Bus::new(rom.mapper, ppu, apu);
     let mut cpu = core::Cpu::new();
 
-    if args.trace_boot {
+    if trace_boot {
         cpu.enable_tracing(true);
         bus.enable_tracing(true);
     }
 
-    let mut app_state = AppState {
+    AppState {
         bus,
         counter: FpsCounter::new(),
         cpu,
@@ -92,67 +125,64 @@ pub fn main() {
         key_map2: build_keymap_c2(),
         limiter: FpsLimiter::new(60.0),
         logpoint: 1,
-        pattern_window: None,
+        pattern_window: PatternTableWindow::new(),
+        main_window,
         pause: false,
-    };
+    }
+}
 
-    let mut input = WinitInputHelper::new();
+fn update_logic(
+    input: &WinitInputHelper,
+    elwt: &EventLoopWindowTarget<()>,
+    app_state: &mut AppState,
+) {
+    // Close events
+    if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+        // this closes everything, don't really care
+        elwt.exit();
+    } else {
+        map_debug_keys(input, app_state, elwt);
 
-    let _ = event_loop.run(|event, elwt| {
-        if input.update(&event) {
-            // Close events
-            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
-                // this closes everything, don't really care
-                elwt.exit();
-                return;
+        let bus = &mut app_state.bus;
+
+        map_inputs(input, bus.controller1_mut(), &app_state.key_map1);
+        map_inputs(input, bus.controller2_mut(), &app_state.key_map2);
+
+        if !app_state.pause {
+            while !bus.ppu_mut().frame_ready() {
+                app_state.cpu.tick(bus);
             }
-
-            map_debug_keys(&input, &mut app_state, elwt);
-
-            let bus = &mut app_state.bus;
-
-            map_inputs(&input, bus.controller1_mut(), &app_state.key_map1);
-            map_inputs(&input, bus.controller2_mut(), &app_state.key_map2);
-
-            if !app_state.pause {
-                while !bus.ppu_mut().frame_ready() {
-                    app_state.cpu.tick(bus);
-                }
-                bus.ppu_mut().clear_frame_ready();
-
-                app_state.limiter.update();
-            }
-
-            window.request_redraw();
+            bus.ppu_mut().clear_frame_ready();
+            app_state.limiter.update();
         }
 
-        if let Event::WindowEvent {
-            window_id,
-            event: WindowEvent::Resized(size),
-        } = &event
-            && let Some(pattern_window) = app_state.pattern_window.as_mut()
-            && *window_id == pattern_window.window.id()
-        {
-            pattern_window.resize(*size);
-        }
+        app_state.main_window.request_redraw();
+    }
+}
 
+fn pattern_window_event(pattern_window: &mut PatternTableWindow, event: WindowEvent) {
+    match event {
+        WindowEvent::RedrawRequested => {
+            pattern_window.draw();
+        }
+        WindowEvent::Resized(size) => {
+            pattern_window.resize(size);
+        }
+        _ => {}
+    }
+}
+
+fn main_window_event(app_state: &mut AppState, event: WindowEvent) {
+    if event == WindowEvent::RedrawRequested {
         // Draw the current frame
-        if let Event::WindowEvent {
-            event: WindowEvent::RedrawRequested,
-            ..
-        } = event
-        {
-            app_state.bus.ppu_mut().renderer().draw();
+        app_state.bus.ppu_mut().renderer().draw();
 
-            if let Some(pattern_window) = app_state.pattern_window.as_ref() {
-                pattern_window.draw();
-            }
-
-            if let Some(fps) = app_state.counter.drawn() {
-                window.set_title(&format!("Rustines | FPS: {:.1}", fps));
-            }
+        if let Some(fps) = app_state.counter.drawn() {
+            app_state
+                .main_window
+                .set_title(&format!("Rustines | FPS: {:.1}", fps));
         }
-    });
+    }
 }
 
 fn map_debug_keys(
@@ -194,8 +224,8 @@ fn map_debug_keys(
         bus.enable_tracing(true);
     }
 
-    if debug_keys_state.show_pattern_window & app_state.pattern_window.is_none() {
-        app_state.pattern_window = Some(PatternTableWindow::create(bus.mapper_ref(), elwt, 4));
+    if debug_keys_state.show_pattern_window && !app_state.pattern_window.is_displayed() {
+        app_state.pattern_window.show(bus.mapper_ref(), elwt, 4);
     }
 }
 
--- crates/rustines/src/pattern_window.rs
+++ crates/rustines/src/pattern_window.rs
@@ -1,54 +1,74 @@
-use std::sync::Arc;
-
 use pixels::{Pixels, ScalingMode, SurfaceTexture};
 use rustines_core::{Mapper, arch::debug_utils::dump_pattern_tables};
 use winit::{
     dpi::{LogicalSize, PhysicalSize},
     event_loop::EventLoopWindowTarget,
-    window::{Window, WindowBuilder},
+    window::{WindowBuilder, WindowId},
 };
 
-pub struct PatternTableWindow {
-    pub window: Arc<Window>,
+struct PatternTableWindowState {
     pixels: Pixels<'static>,
+    window_id: WindowId,
+}
+
+pub struct PatternTableWindow {
+    state: Option<PatternTableWindowState>,
 }
 
 impl PatternTableWindow {
-    pub fn create(mapper: &dyn Mapper, target: &EventLoopWindowTarget<()>, scale: usize) -> Self {
+    pub fn new() -> Self {
+        PatternTableWindow { state: None }
+    }
+
+    pub fn show(&mut self, mapper: &dyn Mapper, target: &EventLoopWindowTarget<()>, scale: usize) {
         let pattern_width = 256 * scale;
         let pattern_height = 128 * scale;
         let buf = dump_pattern_tables(mapper, scale);
 
         let size = LogicalSize::new(pattern_width as f64, pattern_height as f64);
-        let window = Arc::new(
-            WindowBuilder::new()
-                .with_title("Pattern tables")
-                .with_inner_size(size)
-                .with_min_inner_size(size)
-                .with_resizable(true)
-                .build(target)
-                .unwrap(),
-        );
+        let window = WindowBuilder::new()
+            .with_title("Pattern tables")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .with_resizable(true)
+            .build(target)
+            .unwrap();
+
+        let window_id = window.id();
 
         let window_size = window.inner_size();
-        let surface_texture =
-            SurfaceTexture::new(window_size.width, window_size.height, Arc::clone(&window));
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, window);
         let mut pixels = Pixels::new(pattern_width as u32, pattern_height as u32, surface_texture)
             .expect("Cannot create pixels buffer");
         pixels.set_scaling_mode(ScalingMode::Fill);
 
         pixels.frame_mut().copy_from_slice(&buf);
 
-        PatternTableWindow { window, pixels }
+        self.state = Some(PatternTableWindowState { pixels, window_id });
     }
 
-    pub fn draw(&self) {
-        self.pixels.render().expect("Failed to draw");
+    pub(crate) fn is_event(&self, target_window_id: WindowId) -> bool {
+        match &self.state {
+            Some(PatternTableWindowState { window_id, .. }) => *window_id == target_window_id,
+            None => false,
+        }
     }
 
-    pub fn resize(&mut self, size: PhysicalSize<u32>) {
-        self.pixels
-            .resize_surface(size.width, size.height)
-            .expect("Failed to resize pattern table surface");
+    pub(crate) fn draw(&self) {
+        if let Some(PatternTableWindowState { pixels, .. }) = &self.state {
+            pixels.render().expect("Failed to draw");
+        }
+    }
+
+    pub(crate) fn resize(&mut self, size: PhysicalSize<u32>) {
+        if let Some(PatternTableWindowState { pixels, .. }) = &mut self.state {
+            pixels
+                .resize_surface(size.width, size.height)
+                .expect("Failed to resize pattern table surface");
+        }
+    }
+
+    pub(crate) fn is_displayed(&self) -> bool {
+        matches!(&self.state, Some(PatternTableWindowState { .. }))
     }
 }
